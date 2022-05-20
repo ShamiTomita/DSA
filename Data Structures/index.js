035e9d93c412bd783a83a95b80ef4fae5800fca0
ShamiTomita <shamitomita@gmail.com>
2022-05-19 20:34:22 -0400

    bubble Sort

---
+++ Data Structures/index.js
@@ -0,0 +1,15 @@
+//Why do Data Structures matter???
+Binary Search Trees
+Queues
+Singly Linked Lists
+Binary Heaps
+Hash Tables
+Stacks
+Directed Graphs
+and many more!!!
+
+what do they do?
+they are collection of values, they contatin the relationships of those values,
+and the functions that can be applied to the data
+
+these advanced data structures help with more complex coding problems!
