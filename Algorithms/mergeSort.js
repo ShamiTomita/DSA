7d55f60d81d50e2678f88e573df61517f513e1d3
ShamiTomita <shamitomita@gmail.com>
2022-05-20 16:56:35 -0400

    merge Sort

--- Algorithms/mergeSort.js
+++ Algorithms/mergeSort.js
@@ -1,15 +1,34 @@
+//when you have the two sorted arrays
 function merge(arr1, arr2){
-  let results = [];
-  let i =0;
-  let j = 0;
-  while(i < arr1.length && j < arr2.length){
-    if(arr2[j] > arr1[j]){
-      results.push(arr1[i]);
-      i++;
-    }else{
-      results.push(arr2[j]);
-      j++;
+    let results = [];
+    let i = 0;
+    let j = 0;
+    while(i < arr1.length && j < arr2.length){
+        if(arr2[j] > arr1[i]){
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j])
+            j++;
+        }
     }
-  }
-  return results;
+    //once there are no more items in one of the arrays
+    while(i < arr1.length) {
+        results.push(arr1[i])
+        i++;
+    }
+    while(j < arr2.length) {
+        results.push(arr2[j])
+        j++;
+    }
+    return results;
+}
+
+
+function mergeSort(arr){
+    if(arr.length <= 1) return arr;
+    let mid = Math.floor(arr.length/2);
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+    return merge(left, sright);
 }
