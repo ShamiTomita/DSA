eaf2bce801879184cac68db772f4031c1bf24fee
ShamiTomita <shamitomita@gmail.com>
2022-05-20 16:54:41 -0400

    merg

---
+++ Algorithms/mergeSort.js
@@ -0,0 +1,15 @@
+function merge(arr1, arr2){
+  let results = [];
+  let i =0;
+  let j = 0;
+  while(i < arr1.length && j < arr2.length){
+    if(arr2[j] > arr1[j]){
+      results.push(arr1[i]);
+      i++;
+    }else{
+      results.push(arr2[j]);
+      j++;
+    }
+  }
+  return results;
+}
