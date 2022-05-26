8d8dfec4d0c32e8e732048728ca5a044faeecadd
ShamiTomita <shamitomita@gmail.com>
2022-05-25 21:46:00 -0400

    more definition

--- Data Structures/singlyLinkedLists.js
+++ Data Structures/singlyLinkedLists.js
@@ -3,4 +3,5 @@
 //in an array, each item is indexed
 //a singley linked list has no indices, only a relationship between links
 //each element is a node and references the next one (or null)
-//keep track of the head, the tail and the length 
+//keep track of the head, the tail and the length
+//insertion is easier since we dont have to worry about reindexing items 
