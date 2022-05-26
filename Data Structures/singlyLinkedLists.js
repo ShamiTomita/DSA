fe8dd8492f5ab5b86986d34f24821c1e2fa4d903
ShamiTomita <shamitomita@gmail.com>
2022-05-25 21:47:37 -0400

    class

--- Data Structures/singlyLinkedLists.js
+++ Data Structures/singlyLinkedLists.js
@@ -4,4 +4,16 @@
 //a singley linked list has no indices, only a relationship between links
 //each element is a node and references the next one (or null)
 //keep track of the head, the tail and the length
-//insertion is easier since we dont have to worry about reindexing items 
+//insertion is easier since we dont have to worry about reindexing items
+//random access is not allowed as a consequence
+class Node{
+  constructor(val){
+    this.val = val;
+    this.next = null;
+  }
+}
+
+let first = new Node("hi");
+first.next = new Node("there");
+first.next.nex = new Node("how");
+//this is a bad way of constructing a singly linked list
