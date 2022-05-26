997469280af42a77d9676ea49f116c4fa3f80db5
ShamiTomita <shamitomita@gmail.com>
2022-05-25 21:39:51 -0400

    definition

---
+++ Data Structures/singlyLinkedLists.js
@@ -0,0 +1,6 @@
+//singley linked list and how is it different from an array
+//stores any sort of data, ordered
+//in an array, each item is indexed
+//a singley linked list has no indices, only a relationship between links
+//each element is a node and references the next one (or null)
+//keep track of the head, the tail and the length 
