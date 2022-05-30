78d3b7b45d11b32752e9b64c469bdd933b276862
ShamiTomita <shamitomita@gmail.com>
2022-05-30 13:06:30 -0400

    doubly linked list

---
+++ Data_Structures/doublyLinkedList.js
@@ -0,0 +1,19 @@
+//nodes point in two directions
+//more flexibility  === more memory requirements
+class Node{
+  constructor(value){
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+  }
+}
+
+
+
+class DoublyLinkedList {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
