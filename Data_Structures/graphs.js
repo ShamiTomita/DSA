842bf728e12597185c47c99249bc46d58485c3b1
ShamiTomita <shamitomita@gmail.com>
2022-05-31 19:18:20 -0400

    Graph Class

--- Data_Structures/graphs.js
+++ Data_Structures/graphs.js
@@ -17,3 +17,29 @@
 //weighted - each edge has no value assoicated
 //unweighted - assign a value to the edge
 //google maps would be a directed weighted graph
+
+//Adjacency Matrix
+
+//Adjacency List -> use  hashtable/map
+//the values would hold the edges of the vertices
+//adjacency can take up less space in sparce graphs
+//adjacency faster to iterate over all edges
+//adjacency can be slower to look up specific edge so martix better for looking up
+
+//REAL WORLD DATA TENDS TO BE SPARSE SO USE ADJACENCY LIST
+
+
+class Graph{
+  constructor(){
+    this.adjacencyList = {}
+  }
+  addVertex(vertex){
+    if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+  }
+  addEdge(vertex1, vertex2){
+    if(!!this.adjacencyList[vertex1] & !!this.adjacencyList[vertex2]){
+    this.adjacencyList[vertex1].push(vertex2)
+    this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+}
