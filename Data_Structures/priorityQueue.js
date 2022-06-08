4c4bc11fd7b222f5c06fd1ed6a6627a25c59ed61
ShamiTomita <shamitomita@gmail.com>
2022-06-08 11:22:45 -0400

    Dynamic programming

--- Data_Structures/priorityQueue.js
+++ Data_Structures/priorityQueue.js
@@ -3,71 +3,75 @@
 //take one elment at a time
 //abstract concept, not necessarily a binary heap
 //insertion/removal with binaray heap with be logn
-
-class PriorityQueue{
-  constructor(){
-    this.values = []; //all we need structurally
-  }
-  enqueue(value, priority){
-    let newNode = new Node(value, priority)
-    this.values.push(newNode);
-    this.bubbleUp();
-  }
-  bubbleUp(){
-    let index = this.values.length -1;
-    const element = this.values[index];
-    while(index > 0){
-      let parentIndex = Math.floor((index-1)/2);
-      let parent = this.values[parentIndex];
-      if(element.priority >= parent.priority) break;
-      this.values[parentIndex] = element;
-      this.values[index] = parent;
-      index = parentIndex;
+class PriorityQueue {
+    constructor(){
+        this.values = [];
     }
-  }
-  dequeue(){
-    const min = this.values[0];
-    const end = this.values.pop();
-    if(this.values[0] = end);{
-      this.sinkDown();
+    enqueue(val, priority){
+        let newNode = new Node(val, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
     }
-      return max;
-  }
-
-  sinkDown(){
-    let index = 0;
-    const length = this.values.length;
-    const element = this.values[0];
-    while(true){
-      let leftChildIndex = 2 * index + 1;
-      let rightChildIndex = 2 * index + 2;
-      let leftChild, rightChild;
-      let swap = null;
-
-      if(leftChildIndex <length){
-        leftChild = this.values[leftChildIndex];
-        if(leftChild.priority < element.priority){
-          swap = leftChildIndex
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element.priority >= parent.priority) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
         }
-      }
-      if(rightChildIndex < length){
-        rightChild = this.values[rightChildIndex];
-        if((swap === null && rightChild.priority < element.priority)|| (swap !== null && rightChild.priority < leftChild.priority))
-        {
-          swap = rightChildIndex
-        }
-      }
     }
-  }
+    dequeue(){
+        const min = this.values[0];
+        const end = this.values.pop();
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return min;
+    }
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild,rightChild;
+            let swap = null;
+
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx];
+                if(
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                   swap = rightChildIdx;
+                }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
-class Node{
-  constructor(value, priority){
-    this.value = value;
-    this.priority = priority;//this is what matters
-  }
+class Node {
+    constructor(val, priority){
+        this.val = val;
+        this.priority = priority;
+    }
 }
-
 let ER = new PriorityQueue();
 ER.enqueue("common cold", 5);
 ER.enqueue("gunshot wound", 1);
---
+++ DynamicProgramming/intro.js
@@ -0,0 +1,7 @@
+//Dynamic Programming
+//a method for solving a complex problems by breaking down into a
+//colection of simpler subproblems, solving each of those subproblems just once,
+//and storing their solutions
+
+//Overlapping Subproblems (solved with recursion)
+//Optimal Substructure 
