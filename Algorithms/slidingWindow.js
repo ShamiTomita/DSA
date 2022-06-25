87a111d7dd624a5006456ec58cbd3a1bbb1ad6e6
ShamiTomita <shamitomita@gmail.com>
2022-06-25 17:52:04 -0400

    dynamic sliding window

---
+++ Algorithms/slidingWindow.js
@@ -0,0 +1,34 @@
+//fixed!!!
+function maxSubarraySum(arr, num){
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+//Dynamic
+//start small and get maximum then retract, its like a catterpillar
+
+function dynamicSmallestSubarray(arr){
+  let targetSum;
+  let minWindowSize = 0;
+  let currentWindowSum =0;
+  let windowStart =0;
+  for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+    while(currentWindowSum >= targetSum){
+      minWindowSize = Math.min(minWindowSize, windowEnd - windowStart + 1);
+      currentWindowSum -= arr[windowStart]; //removing it from the end, the shrinking process
+      windowStart++; //moving it right
+    }
+  }
+  return minWindowSize;
+}
